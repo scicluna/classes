dfffc856ea738bff675035e8842d6c98314e394f
scicluna <88948700+scicluna@users.noreply.github.com>
2022-11-25 19:57:37 -0500

    mostly working now

--- script.js
+++ script.js
@@ -1,66 +1,158 @@
 class Diceroller {
-    constructor(inputString) {
+    constructor(inputString, newResult, oldResult) {
         this.inputString = inputString
+        this.newResult = newResult
+        this.oldResult = oldResult
         this.clear();
     }
 
+    //clears all of our attributes
     clear(){
         this.inputString = ''
-        this.dicesize = ''
-        this.dicequantity = ''
-        this.result = ''
-        this.lastresult = ''
-        this.operation = undefined
-        newResult.innerHTML = ''
+        this.diceArrays = []
+        this.arraysDiceArrays = []
+        this.operationArray = []
+        this.rolledDice = []
+        this.lastrolledDice = []
+        this.totaloftotals = []
+        this.newerResult = ''
+        this.olderResult = ''
+        this.newResult.innerText = ''
+        this.oldResult.innerText = ''
     }
 
-    roll(){
-        let diceArray = []
-        for (let i=0;i<this.dicequantity;i++){
-            let randomDice = Math.ceil(Math.random()*this.dicesize)
-            diceArray.push(randomDice);
+     //updates our "this.inputString to equal what's typed into the form"
+     update(){
+        this.inputString = inputString.value;
     }
-    console.log(diceArray)
-    this.result = diceArray
-}
-
+    
+    //splits our string into an array of dice arrays and an array of operations
     splitDice(){
-      let array = this.inputString.split('d')
-      console.log(array)
-      this.dicequantity = array[0]
-      this.dicesize = array[1] 
+        this.diceArrays = this.inputString.split(/[-+*\/]/)
+        for (let i=0;i<this.inputString.length;i++){
+            if (this.inputString[i] === '-' || this.inputString[i] === '+' || this.inputString[i] === '*' || this.inputString[i] === '/'){
+                this.operationArray.push(this.inputString[i])
+            }
+        }
     }
 
-    storeDice() {
+    //splits our xdy, xdy array into seperate arrays
+    splitArray(){
+        let cutSplit;
+        let notcutSplit;
+        for (let i = 0; i<this.diceArrays.length; i++){
+            if (this.diceArrays[i].includes('d')){
+                cutSplit = this.diceArrays[i].split('d')
+                this.arraysDiceArrays.push(cutSplit)
+            } 
+            else {
+                notcutSplit = [this.diceArrays[i]]
+                this.arraysDiceArrays.push(notcutSplit)
+            }
 
+        }
     }
 
-    chooseOperation(operation){
-        
+    //rolls based on our arrayDiceArrays properties
+    roll(){
+        for (let i =0; i<this.arraysDiceArrays.length;i++){
+        let size = this.arraysDiceArrays[i][1]
+        let quantity = this.arraysDiceArrays[i][0]
+        let drop = this.arraysDiceArrays[i][2]
+        this.rollDice(size, quantity, drop);
+        }
     }
 
-    compute(){
+    //helper function to roll our dice
+    rollDice(size, quantity, drop){
+        console.log(this.arraysDiceArrays)
+        let heldDice = []
+
+        for (let i =0; i<quantity;i++){
             
+        if (size !== undefined){
+        let randomDice = Math.ceil(Math.random()*size)
+        heldDice.push(randomDice);
+        }}
+    
+        if (size === undefined) {
+        let loner = parseInt(quantity)
+        heldDice.push(loner); 
+        }
+    
+        if (drop != undefined){
+            heldDice = heldDice.sort((a,b) => b-a).slice(0,heldDice.length-drop)
+        } else {
+            heldDice = heldDice.sort((a,b) => b-a)
+        }
+
+        this.rolledDice.push(heldDice);
+        console.log(heldDice)
     }
 
-    update(){
-        this.inputString =  inputString.value
-}
-    printResult(){
-        oldResult.innerHTML = this.lastresult
-        newResult.innerHTML = this.result
-        this.lastresult = this.result
-        this.result = ''
+    //individualy finds the sum of each dice array
+    sumArrays(){
+        let total = 0
+        for (let i=0;i<this.rolledDice.length;i++){
+            for (let j=0; j<this.rolledDice[i].length;j++){
+                total = total + this.rolledDice[i][j]
+            }
+            this.totaloftotals.push(total);
+            total = 0;
+        }
+    }
+
+    //will either add, subtract, multiply, or divide the arrays into each other
+    compute(){
+        let computed = 0
+        for (let i =0; i<this.totaloftotals.length;i++){
+            if (this.operationArray[i] === '+' && this.totaloftotals[i+1] !== undefined){
+                computed = this.totaloftotals[i] + this.totaloftotals[i+1]
+                this.totaloftotals[i+1] = computed
+            }
+            if (this.operationArray[i] === '-' && this.totaloftotals[i+1] !== undefined){
+                computed = parseInt(this.totaloftotals[i]) - parseInt(this.totaloftotals[i+1])
+                this.totaloftotals[i+1] = computed
+            }
+            if (this.operationArray[i] === '*' && this.totaloftotals[i+1] !== undefined){
+                computed = (this.totaloftotals[i]) * (this.totaloftotals[i+1])
+                this.totaloftotals[i+1] = computed
+            }
+            if (this.operationArray[i] === '/' && this.totaloftotals[i+1] !== undefined){
+                computed = this.totaloftotals[i] / this.totaloftotals[i+1]
+                this.totaloftotals[i+1] = computed
+            }
+                this.newerResult = computed
+            
+        }
+    }
+
+    //update our DOMs with the new results and old results
+    updateDom(){
+        this.newResult.innerText = `${this.rolledDice} ${this.newerResult}`;
+        this.oldResult.innerText = `${this.lastrolledDice} ${this.olderResult}`;
+    }
+
+    //getting us ready for the next click
+    cleanup(){
+        this.olderResult = this.newerResult
+        this.newerResult = ''
+        this.lastrolledDice = this.rolledDice
+        this.rolledDice = []
+        this.diceArrays = []
+        this.arraysDiceArrays = []
+        this.operationArray = []
+        this.totaloftotals = []
     }
 }
 
 const clearButton = document.querySelector('[data-clear')
-let newResult = document.querySelector('[data-new-result]')
+const newResult = document.querySelector('[data-new-result]')
 const oldResult = document.querySelector('[data-old-result]')
 const rollButton = document.querySelector('[data-roll]')
 const inputString = document.querySelector('[data-input]')
 
-const diceroller = new Diceroller(inputString)
+const diceroller = new Diceroller(inputString, newResult, oldResult)
 
 clearButton.addEventListener('click', button => {
     console.log('clearing...')
@@ -71,6 +163,10 @@ rollButton.addEventListener('click', button => {
     console.log("rolling...")
     diceroller.update();
     diceroller.splitDice();
+    diceroller.splitArray();
     diceroller.roll();
-    diceroller.printResult();
+    diceroller.sumArrays();
+    diceroller.compute();
+    diceroller.updateDom();
+    diceroller.cleanup();
 })
